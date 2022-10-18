9da41dcdb06e05a0b76d4162067774e3feafec60
Andrey <s.s.andriy@student.khai.edu>
2022-10-18 20:01:35 +0300

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,5 +1,7 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     String botName;
     int yearOfCreation;
@@ -10,6 +12,16 @@ public class ChatBot {
     }
     public void sayHi(){
         System.out.printf("Hello! My name is %s\n", this.botName);
-        System.out.printf("I was created in %d", this.yearOfCreation);
+        System.out.printf("I was created in %d\n", this.yearOfCreation);
+    }
+    public void askName(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, remind me your name.");
+        String name = scanner.nextLine();
+        if (name.length() < 2) {
+            return;
+        }
+        System.out.printf("What a great name you have, %s", name);
+
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         ChatBot chatBot = new ChatBot("Elena", 2022);
         chatBot.sayHi();
+        chatBot.askName();
     }
 }
