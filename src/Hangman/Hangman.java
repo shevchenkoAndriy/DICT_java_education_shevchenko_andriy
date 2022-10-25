49aced1692ffda113d74be86930b2831264cef6d
Andrey <s.s.andriy@student.khai.edu>
2022-10-25 19:29:07 +0300

    Hangman 2-nd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,11 +1,28 @@
 package Hangman;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Hangman {
 
     public void showAnnouncement(){
-        System.out.println("""
-                HANGMAN
-                The game will be available soon.""");
+        System.out.println("HANGMAN");
+    }
+    private void showResultGame(Boolean isWin){
+        if (isWin){
+            System.out.println("You survived!");
+        }
+        else {
+            System.out.println("You lost!");
+        }
     }
 
+    public void startSimpleMod(String guessWord){
+        showAnnouncement();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Guess the word: > ");
+        String userInput = input.nextLine();
+        boolean isWin = Objects.equals(guessWord, userInput);
+        showResultGame(isWin);
+    }
 }
