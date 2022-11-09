bc5be6eba90589ec7a318ac6a47203a897a0caa9
Andrey <s.s.andriy@student.khai.edu>
2022-11-09 22:31:25 +0200

    CoffeeMachine 4-th stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,50 +1,149 @@
 package CoffeeMachine;
 
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class CoffeeMachine {
-    private final Map<String, Integer> ingredientsForOne = Map.of(
-            "water", 200,
-            "milk", 50,
-            "coffee", 15
-    );
+    private int water;
+
+    private int milk;
+
+    private int grain;
+
+    private int cups;
+
+    private int money;
+
+    private final Set<String> actions = Set.of("buy", "fill", "take");
 
     private final Scanner scanner = new Scanner(System.in);
 
-
-    private void showSteps(){
-        System.out.println("""
-                Starting to make a coffee
-                Grinding coffee beans
-                Boiling water
-                Mixing boiled water with crushed coffee beans
-                Pouring coffee into the cup
-                Pouring some milk into the cup
-                Coffee is ready!""");
+    public CoffeeMachine(int water, int milk, int grain, int cups, int money){
+        this.water = water;
+        this.milk = milk;
+        this.grain = grain;
+        this.cups = cups;
+        this.money = money;
     }
-    public void start(){
-        int cups = correctInputInt("Write how many cups of coffee you will need: > ");
-        int maxCups = calcMaxCups();
+    static class Coffee{
+        private static final Map<String, Integer> espresso = Map.of(
+                "water", 250,
+                "grain", 16,
+                "price", 4
+        );
 
-        if (maxCups == cups){
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (maxCups > cups) {
-            System.out.printf("Yes, I can make that amount of coffee and even %d more than that\n", maxCups - cups);
+        private static final Map<String, Integer> latte = Map.of(
+                "water", 350,
+                "milk", 75,
+                "grain", 20,
+                "price", 7
+        );
+
+        private static final Map<String, Integer> cappuccino = Map.of(
+                "water", 200,
+                "milk", 100,
+                "grain", 12,
+                "price", 6
+        );
+
+        private static final Map<String, Map<String, Integer>> actions = Map.of(
+                "1", espresso,
+                "2", latte,
+                "3", cappuccino);
+    }
+
+    private void showCurrentState(){
+        System.out.printf("""
+        The coffee machine has:
+        %d of water
+        %d of milk
+        %d of coffee beans
+        %d of disposable cups
+        %d of money
+        """,
+                this.water,
+                this.milk,
+                this.grain,
+                this.cups,
+                this.money);
+    }
+
+    public void menu(){
+        showCurrentState();
+        String action = selectActions("Write action (buy, fill, take): > ", this.actions);
+        
+        if (Objects.equals(action, "buy")){
+            saleMode();
+        } else if (Objects.equals(action, "fill")) {
+            fillMode();
+        } else if (Objects.equals(action, "take")) {
+            takeMode();
         }
         else {
-            System.out.printf("No, I can make only %d cups of coffee.", cups);
             return;
         }
-        showSteps();
+        menu();
     }
+
+    private void takeMode() {
+        System.out.printf("Success. Coffee machine gave %d money\n", this.money);
+        this.money = 0;
+    }
+
+    private void fillMode() {
+        this.water += correctInputInt("Write how many ml of water you want to add: > ");
+        this.cups += correctInputInt("Write how many disposable coffee cups you want to add:> ");
+        this.milk += correctInputInt("Write how many ml of milk you want to add: > ");
+        this.grain += correctInputInt("Write how many grams of coffee beans you want to add: > ");
+    }
+
+    private void saleMode() {
+
+        String selectedActions = selectActions("What do you want to buy?" +
+                        " 1 - espresso, 2 - latte, 3 - cappuccino: > ",
+                Coffee.actions.keySet());
+
+        Map<String, Integer> selectedCoffee = Coffee.actions.get(selectedActions);
+
+        if (!isExistIngredients(selectedCoffee)){
+            return;
+        }
+
+        sellCoffee(selectedCoffee);
+    }
+
+    private boolean isExistIngredients(Map<String, Integer> needIngredients){
+
+            if (this.water < needIngredients.get("water")) {
+                System.out.println("Sorry, not enough water!");
+                return false;
+            } else if (this.grain < needIngredients.get("grain")) {
+                System.out.println("Sorry, not enough beans!");
+                return false;
+            } else if (this.milk < needIngredients.getOrDefault("milk", 0)) {
+                System.out.println("Sorry, not enough milk!");
+                return false;
+
+            } else if (this.cups < 1) {
+                System.out.println("Sorry, not enough cups!");
+                return false;
+            }
+            return true;
+    }
+
+    private void sellCoffee(Map<String, Integer> selectedCoffee){
+        this.water -= selectedCoffee.get("water");
+        this.milk -= selectedCoffee.getOrDefault("milk", 0);
+        this.grain -= selectedCoffee.get("grain");
+        this.money += selectedCoffee.get("price");
+        this.cups -= 1;
+    }
+
     private int correctInputInt(String label){
         System.out.print(label);
 
         while (true) {
-            Scanner input = new Scanner(System.in);
             try {
-                return input.nextInt();
+                return scanner.nextInt();
             }
             catch (java.util.InputMismatchException e) {
                 scanner.next();
@@ -53,25 +152,16 @@ public class CoffeeMachine {
         }
     }
 
-    private int calcMaxCups(){
-        int availableWater = correctInputInt("Write how many ml of water the coffee machine has: > ");
-        int availableMilk = correctInputInt("Write how many ml of milk the coffee machine has: > ");
-        int availableGrain = correctInputInt("Write how many grams of coffee beans the coffee machine has: > ");
-        int maxCups = 0;
+    private String selectActions(String label, Collection<String> actions){
+        System.out.print(label);
         while (true){
-            if (availableWater < ingredientsForOne.get("water")){
-                break;
-            } else if (availableMilk < ingredientsForOne.get("milk")) {
-                break;
-            } else if (availableGrain < ingredientsForOne.get("coffee")) {
-                break;
+           String selectedAction = scanner.nextLine();
+            if (actions.contains(selectedAction)){
+                return selectedAction;
             }
-            availableWater -= ingredientsForOne.get("water");
-            availableMilk -= ingredientsForOne.get("milk");
-            availableGrain -= ingredientsForOne.get("coffee");
-            maxCups +=1;
+            else {
+                System.out.print("Please input correct actions > ");
+            }
         }
-        return maxCups;
-
     }
 }
--- src/CoffeeMachine/Main.java
+++ src/CoffeeMachine/Main.java
@@ -3,8 +3,8 @@ package CoffeeMachine;
 public class Main {
 
     public static void main(String[] args) {
-        CoffeeMachine coffeeMachine = new CoffeeMachine();
-        coffeeMachine.start();
+        CoffeeMachine coffeeMachine = new CoffeeMachine(400, 540, 120, 9, 550);
+        coffeeMachine.menu();
     }
 
 }
